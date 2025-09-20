25a8a76f44a26457da125225cc7dc87645c1abe2
mevinod.com <connect@mevinod.com>
2025-09-20 19:24:56 +0000

    revamp inlding binary - re-org

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,7 +7,6 @@ def test_get_token_missing_keys():
     client = app.test_client()
     response = client.post("/get-token", json={})
     data = response.get_json()
-
     assert response.status_code == 400
     assert "error" in data
     assert data["error"] == "Missing AWS credentials"
@@ -20,6 +19,6 @@ def test_get_token_with_dummy_keys():
         "region": "us-east-1"
     })
     data = response.get_json()
-
     assert response.status_code in [400, 500]
     assert "error" in data
+
